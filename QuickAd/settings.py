f2e0cd24d00bf12068e55a1071b4b2e8ad700cc2
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2020-05-22 21:11:19 +0600

    Added requirements for deployment

--- QuickAd/settings.py
+++ QuickAd/settings.py
@@ -87,11 +87,17 @@ USE_L10N = True
 
 USE_TZ = True
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = "accounts.User"
 
