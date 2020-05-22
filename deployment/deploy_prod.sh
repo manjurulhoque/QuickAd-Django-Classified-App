97e4ff83cdd6ecedf93ccce9f3dcef11c5d2f46c
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2020-05-23 01:39:31 +0600

    Testing

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 ssh root@103.108.140.130 <<EOF
-  cd /var/www/html/adsproject/classified/
+  cd /var/www/html/jobsproject/jobsportal/
   git pull
-  cd /var/www/html/adsproject/
-  source adsenv/bin/activate
-  cd classified/
+  cd /var/www/html/jobsproject/
+  source jobsenv/bin/activate
+  cd jobsportal/
   cat requirements.txt | xargs -n 1 pip install
   python manage.py migrate
   sudo systemctl restart gunicorn
