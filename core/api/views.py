0a188d8b440e21aadb4e5663f6f57f319447238f
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2020-05-23 16:16:15 +0600

    Remove filter

--- core/api/views.py
+++ core/api/views.py
@@ -9,8 +9,8 @@ from .filters import *
 class AdListAPIView(ListAPIView):
     serializer_class = AdSerializer
     queryset = serializer_class.Meta.model.objects.all()
-    filter_backends = (filters.DjangoFilterBackend,)
-    filterset_class = AdFilter
+    # filter_backends = (filters.DjangoFilterBackend,)
+    # filterset_class = AdFilter
 
 
 class CategoryListAPIView(ListAPIView):
